21b1f6e2366e79c0c52d98d0caabcfad5d3cdb86
luciferMorningStar <41140264+pachiyappanVelan@users.noreply.github.com>
2018-07-12 14:56:13 +0530

    Create Check_VowelOrConsonant.py

---
+++ Check_VowelOrConsonant.py
@@ -0,0 +1,13 @@
+def Check_VowelOrConsonant(character):
+    if character in 'aeiou':
+        print("Vowels")
+    else:
+        print("Consonant")        
+try:
+    input_ = input().strip()
+    if input_.isalpha() and len(input_) == 1:
+        Check_VowelOrConsonant(input_)
+    else:
+        raise
+except:
+    print("Invalid")
